f257c322d0d286a7757933105dbc22b5fab182b2
Ughanthi <usri587@aucklanduni.ac.nz>
2023-04-02 21:32:08 +1200

    added our product section for static page

--- trendlyzer/trendlyzer-frontend/src/App.tsx
+++ trendlyzer/trendlyzer-frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Who from './components/Landing/Who/Who';
+import Ourprod from './components/Landing/Our products/OurProd';
 
 function App() {
   return (
     <div className="App">
       <Who></Who>
+      <Ourprod></Ourprod>
     </div>
+    
   );
 }
 
 export default App;
+
