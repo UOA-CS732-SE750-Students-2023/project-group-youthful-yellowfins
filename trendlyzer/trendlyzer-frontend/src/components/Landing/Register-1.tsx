6145bdf2a3b73452f8bfd9d1357c6f2c248646c8
usri587 <usri587@aucklanduni.ac.nz>
2023-04-25 19:31:45 +1200

    Register and login component

---
+++ trendlyzer/trendlyzer-frontend/src/components/Landing/Register-1.tsx
@@ -0,0 +1,93 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+export default function SignUp() {
+  const handleSubmit = (event : any) => {
+    event.preventDefault();
+    // const data = new FormData(event.currentTarget);
+    // console.log({
+    //   email: data.get("email"),
+    //   password: data.get("password"),
+    // });
+  };
+
+  return ( 
+    
+    <>
+      <Box
+        sx={{  
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          /> 
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel 
+            control={<Checkbox value="remember" color="success" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, background:'#800080' }}
+          >
+            Sign Up
+          </Button>
+          <Grid container>
+        
+            <Grid item>
+              <Link href="/login" variant="body2" style={{ textDecoration:'#800080', color: '#800080'}}>
+                {'Already have an account?, sign in'}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
+}
